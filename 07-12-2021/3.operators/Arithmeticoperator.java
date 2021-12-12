753afbe2462afd3a263ac85d53630dd2bc4fa780
Saravanan V <90813730+SaravananKarthik@users.noreply.github.com>
2021-12-12 19:51:28 +0530

    Add files via upload

---
+++ 07-12-2021/3.operators/Arithmeticoperator.java
@@ -0,0 +1,17 @@
+package test;
+
+public class Arithmeticoperator {
+
+	public static void main(String[] args) {
+		int n=5;
+		int m=2;
+		//m+=n;
+		
+		System.out.println(n+m);
+		System.out.println(n-m);
+		System.out.println(n*m);
+		System.out.println(n/m);
+		System.out.println(n%m);
+
+}
+}
---
+++ 08-12-2021/4.command line argument invalid integers/Countofinvalidintegers.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Countofinvalidintegers {
+	public static void main(String[] values) {
+		int sum = 0;
+		System.out.println("Calculates Sum for below Integers");
+		for(int i=0;i<values.length;i++){
+			System.out.println(values[i]);
+			sum = sum + Integer.parseInt(values[i]);
+		}
+		System.out.println("Sum :" + sum);
+	}
+}
+
